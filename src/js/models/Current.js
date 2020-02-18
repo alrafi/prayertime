b1c70f53123c02a7dc96bcd38efba445d533ae46
alrafi <hafisalrafi@gmail.com>
2020-02-18 15:44:33 +0700

    update getDate method

--- src/js/models/Current.js
+++ src/js/models/Current.js
@@ -30,22 +30,22 @@ export default class Current {
     }
   }
 
-  async getCityName() {
-    try {
-      const res = await axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?lat=${this.coord[0]}&lon=${this.coord[1]}&appid=f3bfa42f5eb3ca462fa3a4e43d1366ff`
-      );
-      this.city = res.data.name;
-      this.country = res.data.sys.country;
-    } catch (err) {
-      console.log(`something error: ${err}`);
-    }
-  }
+  // async getCityName() {
+  //   try {
+  //     const res = await axios.get(
+  //       `http://api.openweathermap.org/data/2.5/weather?lat=${this.coord[0]}&lon=${this.coord[1]}&appid=f3bfa42f5eb3ca462fa3a4e43d1366ff`
+  //     );
+  //     this.city = res.data.name;
+  //     this.country = res.data.sys.country;
+  //   } catch (err) {
+  //     console.log(`something error: ${err}`);
+  //   }
+  // }
 
   getDate() {
     const currentDate = new Date();
     this.day = currentDate.getDate();
-    this.month = currentDate.getMonth();
+    this.month = currentDate.getMonth() + 1;
     this.year = currentDate.getFullYear();
   }
 
