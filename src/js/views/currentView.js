93dbd33e3116ab8afc7a178721b18ca11fb8b17d
alrafi <hafisalrafi@gmail.com>
2020-02-24 16:43:50 +0700

    add method to handle search

--- src/js/views/currentView.js
+++ src/js/views/currentView.js
@@ -53,14 +53,15 @@ export const showNextPrayer = nextPrayer => {
   nextPrayerElement.innerHTML = nextPrayer;
 };
 
+let interval;
+
 export const showCountdown = (nextPrayer, prayerName) => {
   const countdownElement = document.querySelector('.countdown');
 
-  setInterval(() => {
+  const countdown = () => {
     const now = new Date().getTime();
     const distance = nextPrayer - now;
 
-    // const hh = ;
     const hh = ('0' + Math.floor(distance / (1000 * 3600))).slice(-2);
     const mm = (
       '0' + Math.floor((distance % (1000 * 3600)) / (1000 * 60))
@@ -68,7 +69,17 @@ export const showCountdown = (nextPrayer, prayerName) => {
     const ss = ('0' + Math.floor((distance % (1000 * 60)) / 1000)).slice(-2);
 
     countdownElement.innerHTML = `${hh}:${mm}:${ss} left until ${prayerName}`;
-  });
+
+    if (distance < 0) {
+      window.location.reload();
+    }
+  };
+
+  interval = setInterval(countdown, 1000);
+};
+
+export const stopCountdown = () => {
+  clearInterval(interval);
 };
 
 export const highlightNextPrayer = indexNextPrayer => {
@@ -76,10 +87,16 @@ export const highlightNextPrayer = indexNextPrayer => {
   items[indexNextPrayer].classList.add('highlight');
 };
 
+export const removeHighlight = () => {
+  const items = document.querySelectorAll('.item');
+  items.forEach(item => {
+    item.classList.remove('highlight');
+  });
+};
+
 export const showCityName = (city, country) => {
   const cityElement = document.querySelector('.city');
   cityElement.innerHTML = `${city}, ${country}`;
-  // console.log(city, country);
 };
 
 export const showWeather = (temp, icon, description) => {
@@ -93,5 +110,9 @@ export const showWeather = (temp, icon, description) => {
   iconElement.setAttribute('title', `${description}`);
   iconElement.setAttribute('alt', `${description}`);
   tempElement.innerHTML = `${temp} &#176C`;
-  // console.log(tempElement);
+};
+
+export const clearSearch = () => {
+  const search = document.querySelector('.input-city');
+  search.value = '';
 };
