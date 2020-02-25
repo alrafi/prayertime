074783ac689f2b8612a8904339b9daed1d905b35
alrafi <hafisalrafi@gmail.com>
2020-02-25 23:40:36 +0700

    add base for elements html

---
+++ src/js/views/base.js
@@ -0,0 +1,7 @@
+export const elements = {
+  fajr: document.querySelector('.fajr'),
+  dhuhr: document.querySelector('.dhuhr'),
+  asr: document.querySelector('.asr'),
+  maghrib: document.querySelector('.maghrib'),
+  isha: document.querySelector('.isha')
+};
